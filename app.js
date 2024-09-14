48fba4cfe28dde3a3608e5081d5b4bebede0e82d
Samuel <samuferfort@gmail.com>
2024-09-14 17:13:16 +0200

    add security middlewares

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express from "express";
 import logger from "morgan";
 import "dotenv/config";
 import cors from "cors";
-import authRouter from "./routes/authRouter";
-import postsRouter from "./routes/postsRouter"
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import compression from "compression";
 
 const app = express();
 
 app.use(logger("dev"));
+app.use(helmet());
+app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -18,10 +22,26 @@ app.use(
   })
 );
 
-app.use("/auth", authRouter);
-app.use("/post", postsRouter);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use("/api", limiter);
 
+import authRouter from "./routes/authRouter";
+import postsRouter from "./routes/postsRouter";
 
+app.use("/api/auth", authRouter);
+app.use("/api/post", postsRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production" ? "Something broke!" : err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+});
 const PORT = process.env.PORT || "3000";
 
 app.listen(PORT, "0.0.0.0", () => {
